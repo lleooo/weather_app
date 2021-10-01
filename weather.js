a3a10474421eefa0b067dbd92c4cf2265099e547
leo <leo88728@gmail.com>
2021-10-01 16:59:11 +0800

    初步版面

--- weather.js
+++ weather.js
@@ -1,4 +1,4 @@
-const APURL = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089?Authorization=CWB-340C63C7-EC48-4AFA-B6C9-FD8FCFA0F6F7&elementName=Wx,T,CI';
+const APURL = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089?Authorization=CWB-340C63C7-EC48-4AFA-B6C9-FD8FCFA0F6F7&elementName=Wx,T,CI,PoP6h';
 
 // fetch(APURL)
 // .then(function(respone){
@@ -12,20 +12,36 @@ async function get_weather(api) {
     const res = await fetch(api);
     const data = await res.json();
     console.log(data.records.locations[0].location);
-    const weather =data.records.locations[0].location;
+    const weather = data.records.locations[0].location;
     show_weather(weather);
 }
 get_weather(APURL);
 
-function show_weather(weather){
-    let city_name=document.getElementById('city_name');
-    let city_temp=document.getElementById('city_temp');
-    let city_wx=document.getElementById('city_wx');
-    city_name.innerHTML=`
+function show_weather(weather) {
+    let future_weather = document.getElementById('future_weather');
+    city_name.innerHTML = `
     <h1>${weather[0].locationName}</h1>`;
-    city_temp.innerHTML=`
-    <h1>${weather[0].weatherElement[1].time[0].elementValue[0].value}`
-    city_wx.innerHTML=`
-    <h1>${weather[0].weatherElement[1].time[0].elementValue[1].value}`
-    
+    city_temp.innerHTML = `
+    <h1>${weather[0].weatherElement[1].time[0].elementValue[0].value}</h1>`;
+    city_wx.innerHTML = `
+    <h1>${weather[0].weatherElement[2].time[0].elementValue[1].value}</h1>`;
+
+    weather[0].weatherElement[1].time.forEach(element => {
+        weather[0].weatherElement[3].time.forEach(ev => {
+            if (element.dataTime.slice(5, -3) === ev.startTime.slice(5, -3)) {
+                const future_item = document.createElement('div')
+                future_item.id = 'future_item';
+                future_weather.appendChild(future_item);
+
+                future_item.innerHTML = `<div id=future_time>${element.dataTime.slice(5, 7)}/${element.dataTime.slice(9, -3)}</div>
+
+                <div id=future_rain>rain:${ev.elementValue[0].value}%</div>
+
+                <div id=future_temp>${element.elementValue[0].value}C</div>
+                         
+                `
+                console.log(ev.elementValue[0].value)
+            }
+        })
+    });
 }
