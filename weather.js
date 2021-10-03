a20f3e54b15b0344a42f5652d93063e82981a076
leo <leo88728@gmail.com>
2021-10-03 15:10:35 +0800

    換縣市功能完成

--- weather.js
+++ weather.js
@@ -1,33 +1,29 @@
 const APURL = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089?Authorization=CWB-340C63C7-EC48-4AFA-B6C9-FD8FCFA0F6F7&elementName=Wx,T,CI,PoP6h';
 
-// fetch(APURL)
-// .then(function(respone){
-//     console.log(respone);
-//     return respone.json();
-// })
-// .then(function(myJason){
-//     console.log(myJason);
-// })
-async function get_weather(api) {
+var city=0;
+
+async function get_weather(api,city){
     const res = await fetch(api);
     const data = await res.json();
-    console.log(data.records.locations[0].location);
-    const weather = data.records.locations[0].location;
+    console.log(data.records.locations[0].location)
+    console.log(data.records.locations[0].location[city]);
+    const weather = data.records.locations[0].location[city];
     show_weather(weather);
 }
-get_weather(APURL);
+
+get_weather(APURL,city);
 
 function show_weather(weather) {
     let future_weather = document.getElementById('future_weather');
     city_name.innerHTML = `
-    <h1>${weather[0].locationName}</h1>`;
+    <h1>${weather.locationName}</h1>`;
     city_temp.innerHTML = `
-    <h1>${weather[0].weatherElement[1].time[0].elementValue[0].value}<span>°C</span></h1>`;
+    <h1>${weather.weatherElement[1].time[0].elementValue[0].value}<span>°C</span></h1>`;
     city_wx.innerHTML = `
-    <h1>${weather[0].weatherElement[0].time[0].elementValue[0].value}</h1>`;
+    <h1>${weather.weatherElement[0].time[0].elementValue[0].value}</h1>`;
 
-    weather[0].weatherElement[1].time.forEach(element => {
-        weather[0].weatherElement[3].time.forEach(ev => {
+    weather.weatherElement[1].time.forEach(element => {
+        weather.weatherElement[3].time.forEach(ev => {
             if (element.dataTime.slice(5, -3) === ev.startTime.slice(5, -3)) {
                 const future_item = document.createElement('div')
                 future_item.id = 'future_item';
@@ -40,8 +36,25 @@ function show_weather(weather) {
                 <div id=future_temp>${element.elementValue[0].value}°</div>
                          
                 `
-                console.log(ev.elementValue[0].value)
             }
         })
     });
+}
+
+var next_page=document.getElementById('next_page');
+
+next_page.addEventListener('click',change_city);
+
+
+function change_city(){
+    city++;
+    let city_name=document.getElementById('city_name');
+    let city_temp=document.getElementById('city_temp');
+    let city_wx=document.getElementById('city_wx');
+    let future_weather = document.getElementById('future_weather');
+    city_name.innerHTML='';
+    city_temp.innerHTML='';
+    city_wx.innerHTML='';
+    future_weather.innerHTML=''
+    get_weather(APURL,city);
 }
